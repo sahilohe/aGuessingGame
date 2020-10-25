0b874e163b93b41801cb10ae000c0bce69667a20
Sahil Ohe <sahilohe97@gmail.com>
2020-10-25 10:05:06 +0530

    Update GuessingGame.java

--- GuessingGame.java
+++ GuessingGame.java
@@ -1,30 +1,37 @@
+package com.fsociety97;
 import java.util.Scanner;
 import java.util.Random;
 
 public class GuessingGame {
 
-	public static void main(String[] args) {
+	public static void main() {
 		Scanner input = new Scanner(System.in);
 		Random randomNumber = new Random();
+		boolean yes = false;
 
 		int guessNumber = randomNumber.nextInt(10);
 		System.out.println(guessNumber);
 		int number = 0;
 
 		while (number < 3) {
-			System.out.println("\nEnter your guess: ");
+			System.out.println("Enter your guess: ");
 			int answer = input.nextInt();
 			number += 1;
 
 			if (answer == guessNumber) {
-				System.out.println("Your guess is right!");
+				System.out.println("\nYou did it!");
+				yes = true;
 				break;
 			} else {
-				System.out.println("You are wrong at guessing numbers.");
+				System.out.println("Nope!\n");
 			}
 		}
-		
+		if (yes) {
+			System.out.println("Your guess is right!");
+		}
+		else {
+			System.out.println("\nYou are done! You are bad at guessing.");
+		}
 		input.close();
 		}
 }
-
