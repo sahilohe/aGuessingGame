c7604160d0bcf853e936fbb8e00ca67ab5a07667
Sahil Ohe <sahilohe97@gmail.com>
2021-07-05 19:54:46 +0530

    Update guessingGame.js

--- guessingGame.js
+++ guessingGame.js
@@ -1,19 +1,27 @@
 // library to take user input. Need to install
 const getInput = require('prompt-sync')(); // npm install promt-sync
 
-for ( let i=0; i < 5;i++) { // user gets 5 chances to play the game
-  randomNumber = Math.round(Math.random() * 10 + 1) // generates a random number
-  userGuess = getInput("What's your guess ? "); // takes user input
+//creating a function
+function guessingGame() {
 
-// condition to check the user input matches the random number generated
-if (userGuess == randomNumber) {
-  console.log("You guessed the number correctly!")
-  console.log("You guessed " + randomNumber + '\n')
+  for ( let i=0; i < 5;i++) {
+    randomNumber = Math.round(Math.random() * 10 + 1) // generates a random number
+    userGuess = getInput("What's your guess ? "); // takes user input
+
+  // condition to check the user input matches the random number generated
+  if (userGuess == randomNumber) {
+    console.log("You guessed the number correctly!")
+    console.log("You guessed " + randomNumber + '\n')
+  }
+
+  else {
+    console.log("You are wrong")
+    console.log("The correct was " + randomNumber + '\n')
+  }
+
+  }
+
 }
 
-else {
-  console.log("You are wrong")
-  console.log("The correct was " + randomNumber + '\n')
-}
-
-}
+// Calling the function
+guessingGame()
